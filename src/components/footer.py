0dfac7bd79a66e4b17dcac569743ee6e18eacd16
anishkumar131 <anishksinha.ak143@gmail.com>
2026-05-22 23:35:02 +0530

    Finishing Touches

--- src/components/footer.py
+++ src/components/footer.py
@@ -7,4 +7,13 @@ def footer_home():
         <p style="font-weight:bold; color:white;"> Created by Anish k. sinha </p>
         </div>
                 """, unsafe_allow_html=True)
+    
+
+def footer_dashboard():
+
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;"> Created by Anish k. sinha </p>
+        </div>
+                """, unsafe_allow_html=True)
         
